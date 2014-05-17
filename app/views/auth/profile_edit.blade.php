978b0329716efe164d8b015ebdb2d520b411a5f0
Chirag Gude <chiraggude@gmail.com>
2014-05-17 15:42:14 +0100

    Allow user to edit username and email

--- app/views/auth/profile_edit.blade.php
+++ app/views/auth/profile_edit.blade.php
@@ -7,24 +7,16 @@
 
 {{Form::model($user, array('action'=> array('AccountController@profileSave','$user->id'),'class' => 'form-horizontal'))}}
 
-    {{ Form::textField('first_name', 'First Name', 'John') }}
+    {{ Form::textField('first_name', 'First Name', '') }}
 
-    {{ Form::textField('last_name', 'Last Name', 'Doe') }}
+    {{ Form::textField('last_name', 'Last Name', '') }}
 
-    <div class="form-group{{ $errors->has('username') ? ' has-error' : null }}">
-        <label class="control-label">Username</label>
-        {{ Form::text('username', null, array('class' => 'form-control', 'disabled')) }}
-        <p class="help-block">{{ $errors->first('username') }}</p>
-    </div>
+    {{ Form::textField('username', 'Username', '') }}
 
-    <div class="form-group{{ $errors->has('email') ? ' has-error' : null }}">
-        <label for="email" class="control-label">Email</label>
-            {{ Form::email('email', null, array('class' => 'form-control', 'disabled')) }}
-            <p class="help-block">{{ $errors->first('email') }}</p>
-    </div>
+    {{ Form::emailField('email', 'Email', '') }}
 
     <div class="form-group">
-            {{ Form::submit('Save Profile', array('class' => 'btn btn-primary')) }}
+    {{ Form::submit('Save Profile', array('class' => 'btn btn-primary')) }}
     </div>
 
 {{ Form::close() }}
