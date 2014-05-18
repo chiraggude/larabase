5baf73a2239e26cfe9ba9f3df8f0bbedeaf1c583
Chirag Gude <chiraggude@gmail.com>
2014-05-18 11:23:20 +0100

    Helper function to set Active state to nav links

--- app/LaraBase/helpers.php
+++ app/LaraBase/helpers.php
@@ -1,6 +1,7 @@
 <?php
 
-function set_active($path, $active='active')
+// Function to set Active States in Navigation Links >views>layouts>navigation.blade.php
+function active($path, $active='active')
 {
     return Request::is($path) ? $active : null;
 }
--- app/views/layouts/navigation.blade.php
+++ app/views/layouts/navigation.blade.php
@@ -7,37 +7,34 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            {{ HTML::link('/', 'LARABASE', array('class' => 'navbar-brand')) }}
+            <a class="navbar-brand" href="{{ URL::to('/') }}"><i class="fa fa-cubes"></i> LARABASE</a>
         </div>
 
         <div class="navbar-collapse collapse">
             <ul class="nav navbar-nav">
-                <li {{ Request::is('reports') ? 'class="active"':null }}><a href="{{ URL::to('reports') }}">Reports</a></li>
+                <li class="{{ active('reports') }}"><a href="{{ URL::to('reports') }}">Reports</a></li>
                 <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Pages <b class="caret"></b></a>
                     <ul class="dropdown-menu">
-                        <li><a href="{{ URL::to('about') }}">About</a></li>
-                        <li><a href="{{ URL::to('contact') }}">Contact</a></li>
-                        <li><a href="#">Something else here</a></li>
+                        <li class="{{ active('about') }}"><a href="{{ URL::to('about') }}">About</a></li>
                         <li class="divider"></li>
-                        <li class="dropdown-header">Nav header</li>
-                        <li><a href="#">Separated link</a></li>
-                        <li><a href="#">One more separated link</a></li>
+                        <li class="dropdown-header">Nav header text</li>
+                        <li class="{{ active('contact') }}"><a href="{{ URL::to('contact') }}">Contact</a></li>
                     </ul>
                 </li>
                 @if ( Auth::user())
-                <li><a href="{{ URL::to('users') }}">Users</a></li>
+                <li class="{{ active('users') }}"><a href="{{ URL::to('users') }}">Users</a></li>
                 @endif
             </ul>
 
             <ul class="nav navbar-nav navbar-right">
                 @if ( Auth::guest())
-                <li><a href="{{ URL::to('login') }}">Login</a></li>
-                <li><a href="{{ URL::to('register') }}">Register</a></li>
+                <li class="{{ active('login') }}"><a href="{{ URL::to('login') }}">Login</a></li>
+                <li class="{{ active('register') }}"><a href="{{ URL::to('register') }}">Register</a></li>
                 @else
-                <li><a href="{{ URL::to('dashboard') }}">Dashboard</a></li>
-                <li><a href="{{ URL::to('profile') }}">Profile</a></li>
-                <li><a href="{{ URL::to('settings') }}">Settings</a></li>
+                <li class="{{ active('dashboard') }}"><a href="{{ URL::to('dashboard') }}">Dashboard</a></li>
+                <li class="{{ active('profile') }}"><a href="{{ URL::to('profile') }}">Profile</a></li>
+                <li class="{{ active('settings') }}"><a href="{{ URL::to('settings') }}">Settings</a></li>
                 <li><a href="{{ URL::to('logout') }}">Logout</a></li>
                 @endif
             </ul>
