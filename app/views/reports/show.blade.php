13f879a412fbf1d5fea708494388fb7813c55b3e
Chirag Gude <chiraggude@gmail.com>
2014-05-19 18:06:45 +0100

    Report Author name in Uppercase

--- app/views/reports/show.blade.php
+++ app/views/reports/show.blade.php
@@ -3,7 +3,7 @@
 <h2>{{ $report->title }} <small>{{ $report->updated_at->diffForHumans() }}</small></h2>
 <hr>
 <p>{{ $report->content }}</p>
-<p class="text-muted">PUBLISHED BY {{ link_to("/users/{$user->username}", $user->username) }} </p>
+<p class="text-muted">PUBLISHED BY {{ link_to("/users/{$user->username}", mb_strtoupper($user->username)) }}</p>
 <hr>
 <a class="btn btn-xs btn-info" href="{{ URL::to('reports/' . $report->id . '/edit') }}">Edit</a>
 {{ Form::open(array('route' => ['reports.destroy', $report->id], 'method' => 'DELETE', 'class' => 'pull-right')) }}
