975de8c646e3c6a9b96b8060c8d9740b6012434f
Chirag Gude <chiraggude@users.noreply.github.com>
2014-06-03 12:42:12 +0100

    Filters for CSRF is now DRY

--- app/routes.php
+++ app/routes.php
@@ -1,11 +1,15 @@
 <?php
 
-// ID's accross all routes should be integers. No need to ->where('id', '\d+')
-// Route::pattern() allows us to define parameter constraints on a global level.
+// ID's accross all routes should be integers (parameter constraints on a global level)
 Route::pattern('id', '\d+');
+
+// Filter every POST, PUT, DELETE request for the CSRF token (Patter based Filter)
+Route::when('*', 'csrf', array('post', 'put', 'delete'));
+
 
 // RESTful resources
 Route::resource('reports', 'ReportsController');
+
 
 // Only Authenticated users can access these routes
 Route::group(['before' => 'auth'], function()
@@ -14,17 +18,14 @@ Route::group(['before' => 'auth'], function()
     Route::get('users/{username}',  'AccountController@profilePublic');
     Route::get('profile',           'AccountController@profile');
     Route::get('settings',          'AccountController@settings');
+    Route::get('password/change',   'AccountController@passwordChange');
+    Route::post('password/change',  'AccountController@passwordSave');
+    Route::get('profile/edit',      'AccountController@profileEdit');
+    Route::post('profile/edit',     'AccountController@profileSave');
     Route::get('users',             'UserController@index');
     Route::get('logout',            'UserController@logout');
-    Route::get('password/change',   'AccountController@passwordChange');
-    Route::get('profile/edit',      'AccountController@profileEdit');
-    // All post routes should be protected from CSRF
-    Route::group(['before' => 'csrf'], function()
-    {
-        Route::post('profile/edit',     'AccountController@profileSave');
-        Route::post('password/change',  'AccountController@passwordSave');
-    });
 });
+
 
 // Admin Routes
 Route::group(['before' => 'auth|admin','prefix' => 'admin'], function()
@@ -36,17 +37,14 @@ Route::group(['before' => 'auth|admin','prefix' => 'admin'], function()
 // Only Guests can access these Routes
 Route::group(['before' => 'guest'], function()
 {
-    Route::get( 'login',                       'UserController@login');
-    Route::get( 'register',                    'UserController@register');
-    Route::get( 'activate/{code}',              array('as'=>'activate', 'uses' => 'UserController@activate'));
-    Route::controller('password',              'RemindersController');
-    // All post routes should be protected from CSRF
-    Route::group(['before' => 'csrf'], function()
-    {
-        Route::post('login',                  'UserController@processLogin');
-        Route::post('register',               'UserController@processRegister');
-    });
+    Route::get( 'login',                  'UserController@login');
+    Route::post('login',                  'UserController@processLogin');
+    Route::get( 'register',               'UserController@register');
+    Route::post('register',               'UserController@processRegister');
+    Route::get( 'activate/{code}',        array('as'=>'activate', 'uses' => 'UserController@activate'));
+    Route::controller('password',         'RemindersController');
 });
+
 
 // Public Routes
 Route::get('feedback',      'HomeController@feedbackShow');
@@ -54,6 +52,7 @@ Route::post('feedback',      'HomeController@feedbackSave');
 Route::get('faqs',        'HomeController@faqs');
 Route::get('about',        'HomeController@about');
 Route::get('/',             'HomeController@home');
+
 
 // Developer Routes
 Route::get('hello',         'DevController@hello');
