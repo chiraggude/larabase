9bc6c204dbc944d61d8f56289836b44891b9ecff
Chirag Gude <chiraggude@gmail.com>
2014-06-06 15:35:00 +0100

    Using new faker numberBetween()

--- app/database/seeds/ReportsTableSeeder.php
+++ app/database/seeds/ReportsTableSeeder.php
@@ -13,7 +13,7 @@ class ReportsTableSeeder extends Seeder {
 		foreach(range(1, 20) as $index)
 		{
             Report::create([
-            'user_id' => $faker->randomNumber(1,3),
+            'user_id' => $faker->numberBetween(1,3),
             'title'=> $faker->paragraph(1),
             'content'=> $faker->paragraph(40),
             'category'=> $faker->word(1),
