c39b3e1eeb75135a78633aa3677cd9f440f37247
Chirag Gude <chiraggude@gmail.com>
2014-08-05 23:27:30 +0100

    Added functionality to resend Activation code

--- app/views/layouts/notifications.blade.php
+++ app/views/layouts/notifications.blade.php
@@ -1,41 +1,49 @@
 @if (Session::has('message'))
-<div class="alert alert-info alert-dismissable">
+<div class="alert alert-info alert-dismissable" id="alert">
     <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
     <strong>{{ Session::get('message') }}</strong>
 </div>
 @endif
 
 @if (Session::has('info'))
-<div class="alert alert-info alert-dismissable">
+<div class="alert alert-info alert-dismissable" id="alert">
     <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
     <strong>{{ Session::get('info') }}</strong>
 </div>
 @endif
 
 @if (Session::has('success'))
-<div class="alert alert-success alert-dismissable">
+<div class="alert alert-success alert-dismissable" id="alert">
     <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
     <strong>{{ Session::get('success') }}</strong>
 </div>
 @endif
 
 @if (Session::has('errors'))
-<div class="alert alert-danger alert-dismissable">
+<div class="alert alert-danger alert-dismissable" id="alert">
     <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
     <strong>{{ HTML::ul($errors->all()) }}</strong>
 </div>
 @endif
 
 @if (Session::has('error'))
-<div class="alert alert-danger alert-dismissable">
+<div class="alert alert-danger alert-dismissable" id="alert">
     <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
     <strong>{{ Session::get('error') }}</strong>
 </div>
 @endif
 
 @if (Session::has('warning'))
-<div class="alert alert-warning alert-dismissable">
+<div class="alert alert-warning alert-dismissable" id="alert">
     <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
     <strong>{{ Session::get('warning') }}</strong>
+</div>
+@endif
+
+@if (Session::has('activation_message'))
+<div class="alert alert-info alert-dismissable">
+    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+    <strong>{{ Session::get('activation_message') }}</strong>
+    <strong><a href="{{ URL::to('resend-activation') }}" class="btn btn-warning btn-xs">Resend Activation Code</a></strong>
 </div>
 @endif
