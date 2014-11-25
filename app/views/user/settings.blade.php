a928d9e38e8bc9c20416f0c61c829da382c2718b
Chirag Gude <chiraggude@gmail.com>
2014-11-25 18:08:28 +0000

    Tweaked "user settings" page

--- app/views/user/settings.blade.php
+++ app/views/user/settings.blade.php
@@ -6,20 +6,37 @@
 <div class="row">
     <div class="col-md-12">
 
-        <h3><i class="fa fa-lock"></i> Security & Privacy</h3>
-        {{ HTML::table($security_settings, ['Setting 1', 'Setting 2', 'Setting 3', 'Setting 4', 'Setting 5']) }}
+      <div role="tabpanel">
+          <!-- Nav tabs -->
+          <ul class="nav nav-tabs" role="tablist">
+            <li role="presentation" class="active">
+                <a href="#security" aria-controls="security" role="tab" data-toggle="tab"><i class="fa fa-lock"></i> Security</a>
+            </li>
+            <li role="presentation">
+                <a href="#general" aria-controls="general" role="tab" data-toggle="tab"><i class="fa fa-exchange"></i> General</a>
+            </li>
+          </ul>
 
-        <h3><i class="fa fa-exchange"></i> Personalisation</h3>
-        {{ HTML::table($personal_settings, ['Timezone', 'Setting 2', 'Setting 3', 'Setting 4', 'Setting 5']) }}
+          <!-- Tab panes -->
+          <div class="tab-content">
+              <div role="tabpanel" class="tab-pane active" id="security">
+                    {{ HTML::table($security_settings, ['Setting 1', 'Setting 2', 'Setting 3', 'Setting 4', 'Setting 5']) }}
+              </div>
+              <div role="tabpanel" class="tab-pane" id="general">
+                     {{ HTML::table($personal_settings, ['Timezone', 'Setting 2', 'Setting 3', 'Setting 4', 'Setting 5']) }}
+              </div>
+          </div>
+        </div>
 
-        {{ HTML::br(2) }}
+        {{ HTML::br(1) }}
 
-        <a href="{{ URL::to('/settings/edit') }}" class="btn btn-default"><i class="fa fa-edit"></i> Edit Settings</a>
-        <a href="{{ URL::to('password/change') }}" class="btn btn-default pull-right"><i class="fa fa-lock"></i> Change Password</a>
+        <a href="{{ URL::to('/settings/edit') }}" class="btn btn-default btn-sm"><i class="fa fa-edit"></i> Edit Settings</a>
+
+        <a href="{{ URL::to('password/change') }}" class="btn btn-default btn-sm pull-right"><i class="fa fa-lock"></i> Change Password</a>
 
         {{ HTML::br(3) }}
 
-        <p class="text-muted">Account created on {{ $user->created_at->setTimezone($user_timezone)->toDayDateTimeString() }} ({{ $user->created_at->diffForHumans() }}) </p>
+        <p class="text-muted">Account created {{ $user->created_at->diffForHumans() }} ({{ $user->created_at->setTimezone($user_timezone)->toDayDateTimeString() }}) </p>
 
         <button class="btn btn-xs btn-default" data-toggle="modal" data-target="#delete-account"><i class="fa fa-power-off"></i> Delete Account</button>
         {{ HTML::deleteAccountModal('delete-account') }}
