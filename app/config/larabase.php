702e89535155f8612dad73359ecaeb10ad3c2807
Chirag Gude <chiraggude@gmail.com>
2014-11-30 14:23:05 +0000

    Added config for maintenance mode

--- app/config/larabase.php
+++ app/config/larabase.php
@@ -5,4 +5,7 @@ return [
     // All Notifications will go to the Admin email by default
     'admin_email' => getenv('ADMIN_EMAIL'),
 
+    // Countdown time will be activated once the app is in maintenance mode
+    'maintenance_time' => getenv('MAINTENANCE_TIME')
+
 ];
